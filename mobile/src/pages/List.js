c274d8e9b5d59ffba6ed2abbc19603e65f17564f
Arli <arli.machado@gmail.com>
2019-10-04 18:00:20 -0300

    Definindo a pagina de listagem de spots

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,8 +1,39 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, Image, StyleSheet, AsyncStorage } from "react-native";
 
+import logo from "../assets/logo.png";
+
+import SpotList from "../components/SpotList";
 // import { Container } from './styles';
 
 export default function List() {
-  return <View />;
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storageTechs => {
+      const techsArray = storageTechs.split(",").map(tech => tech.trim());
+
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <SpotList />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 30
+  }
+});
