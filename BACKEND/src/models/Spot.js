47a9df81e7cc624e600a4ceea22be52fd044e8e8
Arli <arli.machado@gmail.com>
2019-10-07 17:05:51 -0300

    Finalizando a aplicação

--- BACKEND/src/models/Spot.js
+++ BACKEND/src/models/Spot.js
@@ -19,7 +19,7 @@ const SpotSchema = new mongoose.Schema(
 );
 
 SpotSchema.virtual("thumbnail_url").get(function() {
-  return `http://localhost:3333/files/${this.thumbnail}`;
+  return `http://10.168.71.113:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model("Spot", SpotSchema);
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import socketio from "socket.io-client";
 import {
   SafeAreaView,
   ScrollView,
   Image,
   StyleSheet,
-  AsyncStorage
+  AsyncStorage,
+  Alert
 } from "react-native";
 
 import logo from "../assets/logo.png";
@@ -14,6 +16,22 @@ import SpotList from "../components/SpotList";
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://10.168.71.113:3333", {
+        query: { user_id }
+      });
+
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem("techs").then(storageTechs => {
